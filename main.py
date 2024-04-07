8328a14af4a596758dc2b8db87cbaa2f12755b1f
asdt123 <asdt@ufmg@gmail.com>
2024-04-07 19:27:07 -0300

    codigo extremamente bagunçado feito so para teste

--- main.py
+++ main.py
@@ -4,23 +4,8 @@ import pygame
 
 # Import and initialize the pygame library
 import pygame
+from pygame.locals import *
 pygame.init()
-'''SDL_VIDEO_ALLOW_SCREENSAVER = 0'''
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-    K_a,
-    K_s,
-    K_d,
-    K_w,
-    K_f,
-    K_e,
-)
 
 # Set up the drawing window
 screen = pygame.display.set_mode([500, 500])
@@ -34,8 +19,10 @@ f = 0
 k = 0
 pulo = False
 relogio = pygame.time.Clock()
+frames = 30
+sr=100
 while running:
-    relogio.tick(30)
+    relogio.tick(frames)
     pygame.key.set_repeat(10)
     # Did the user click the window close button?
     for event in pygame.event.get():
@@ -47,13 +34,12 @@ while running:
         
         if event.type == KEYDOWN:
             if event.key == K_UP and z==screen.get_height():
-                print(z,'////', screen.get_height())
                 pulo = True
             if event.key == K_DOWN:
                 pygame.time.wait(300)
-            if event.key == K_LEFT and f>(screen.get_width()/2*-1):
+            if event.key == K_LEFT and f>(screen.get_width()//2*-1)+sr:
                 f=f-10
-            elif event.key == K_RIGHT and f<(screen.get_width()/2):
+            elif event.key == K_RIGHT and f<(screen.get_width()//2):
                 f=f+10
             if event.key == K_a and x>1:
                 x=x-1
@@ -67,28 +53,22 @@ while running:
                 k=k-1
             elif event.key == K_e and k<254:
                 k=k+1 
-                print(k)  
     if pulo:
-        if z>int(screen.get_height()/2):
-            pygame.time.delay(1)
-            z=z-20
-            print(z)  
+        if z>screen.get_height()//2:
+            z=z-int(10*60/frames)
         else:
             pulo = False
     else:
         if z<screen.get_height():
-            pygame.time.delay(1)
-            z=z+20
-            print(z) 
+            z=z+int(10*60/frames)
     if z>screen.get_height(): 
         z=screen.get_height()
-        print(z) 
 
-        
+    print(z)
     # Fill the background with white
     screen.fill((255, 255, 255))
     # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (x, y, k), (screen.get_width()/2+f,z), 75)
+    retangulo = pygame.draw.rect(screen, (x,y,k), (screen.get_width()//2+f-sr,z-sr,sr,sr))
 
     # Flip the display
     pygame.display.flip()
