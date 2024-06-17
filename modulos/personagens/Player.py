642a6b27d757c7797fa2fbbec19f7d14ff3cc99e
asdt <asdtufmg@gmail.com>
2024-06-17 17:18:51 -0300

    retirado ajuste lateral e ajustada algumas skins

--- modulos/personagens/Player.py
+++ modulos/personagens/Player.py
@@ -94,13 +94,13 @@ class Player(Nave):
         self.rect.move_ip((0, 0))
       elif self.rect.bottom>screen.get_height() and velocidade[1]>0 and self.rect.left<screen.get_width()//6 and velocidade[0]<0:
         self.rect.move_ip((0, 0))
-      elif self.rect.top<0 and velocidade[1]<0 and self.rect.right>screen.get_width()-screen.get_width()//6 and velocidade[0]>0:
+      elif self.rect.top<0 and velocidade[1]<0 and self.rect.right>screen.get_width()-screen.get_width()//6 - screen.get_width()//90 and velocidade[0]>0:
         self.rect.move_ip((0, 0))
-      elif self.rect.bottom>screen.get_height() and velocidade[1]>0 and self.rect.right>screen.get_width()-screen.get_width()//6  and velocidade[0]>0:
+      elif self.rect.bottom>screen.get_height() and velocidade[1]>0 and self.rect.right>screen.get_width()-screen.get_width()//6 - screen.get_width()//90  and velocidade[0]>0:
         self.rect.move_ip((0, 0))
       elif self.rect.left<screen.get_width()//6 and velocidade[0]<0:
         self.rect.move_ip((0,velocidade[1]))
-      elif self.rect.right>screen.get_width()-screen.get_width()//6 and velocidade[0]>0:
+      elif self.rect.right>screen.get_width()-screen.get_width()//6 - screen.get_width()//90 and velocidade[0]>0:
         self.rect.move_ip((0,velocidade[1]))
       elif self.rect.top<0 and velocidade[1]<0:
         self.rect.move_ip((velocidade[0], 0))
@@ -108,11 +108,6 @@ class Player(Nave):
         self.rect.move_ip((velocidade[0], 0))
       else:
         self.rect.move_ip(velocidade)
-
-        if self.rect.left<screen.get_width()//6:
-          self.rect.left=screen.get_width()//6
-        if self.rect.right>screen.get_width()-screen.get_width()//6:
-          self.rect.right=screen.get_width()-screen.get_width()//6
 
   #metodo para realizar ataque
   def atacar(self)->None:
